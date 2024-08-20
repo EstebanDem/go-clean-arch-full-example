aa5ded7cb7cb9ec274e077ea3019040fed95def6
Esteban Demarchi <83315171+EstebanDem@users.noreply.github.com>
2024-08-20 16:50:40 -0300

    :sparkles: use-case: add employee
    * Add add employee use-case
    
    * Fix asserts
    
    * Add unit tests

---
+++ internal/application/usecases/addemployee_usecase.go
@@ -0,0 +1,61 @@
+package usecases
+
+import (
+	"github.com/google/uuid"
+	"go-clean-arch-example/internal/domain"
+	"time"
+)
+
+type AddEmployeeRequest struct {
+	Name    string
+	Country string
+	Salary  SalaryRequest
+}
+
+type SalaryRequest struct {
+	Currency string
+	Value    float64
+}
+
+type AddEmployeeResponse struct {
+	Id        uuid.UUID
+	CreatedAt time.Time
+}
+
+type AddEmployeeUseCase interface {
+	AddEmployee(request AddEmployeeRequest) (AddEmployeeResponse, error)
+}
+
+type addEmployeeUseCase struct {
+	employeesRepo domain.EmployeeRepository
+	salaryRepo    domain.SalaryRepository
+}
+
+func (uc addEmployeeUseCase) AddEmployee(request AddEmployeeRequest) (AddEmployeeResponse, error) {
+	employee, err := domain.NewEmployee(request.Name, request.Country, request.Salary.Currency, request.Salary.Value)
+	if err != nil {
+		return AddEmployeeResponse{}, err
+	}
+
+	err = uc.salaryRepo.Save(*employee.Salary)
+	if err != nil {
+		return AddEmployeeResponse{}, err
+	}
+
+	err = uc.employeesRepo.Save(*employee)
+	if err != nil {
+		return AddEmployeeResponse{}, err
+	}
+
+	return AddEmployeeResponse{
+		Id:        employee.Id,
+		CreatedAt: employee.CreatedAt,
+	}, nil
+}
+
+func InitEmployeeUseCase(er domain.EmployeeRepository, sr domain.SalaryRepository) AddEmployeeUseCase {
+	return addEmployeeUseCase{
+		employeesRepo: er,
+		salaryRepo:    sr,
+	}
+}
--- internal/domain/salary_test.go
+++ internal/domain/salary_test.go
@@ -8,22 +8,19 @@ import (
 func TestNewSalaryWrongCurrency(t *testing.T) {
 	_, err := NewSalary("-", 260)
 
-	assert.Error(t, ErrInvalidCurrency, err)
+	assert.Equal(t, ErrInvalidCurrency, err)
 }
 
 func TestNewSalaryWrongValue(t *testing.T) {
 	_, err := NewSalary("ARS", 0)
 
-	assert.Error(t, ErrInvalidValue, err)
+	assert.Equal(t, ErrInvalidValue, err)
 }
 
 func TestNewSalary(t *testing.T) {
 	salary, err := NewSalary("ARS", 250)
 
-	if err != nil {
-		t.Errorf("got %q, happy path expected", err.Error())
-	}
-
+	assert.Nil(t, err)
 	assert.Equal(t, salary.Currency, "ARS")
 	assert.Equal(t, salary.Value, 250.0)
 }
