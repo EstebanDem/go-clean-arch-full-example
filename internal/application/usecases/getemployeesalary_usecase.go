1908ee13276e3c45d7fa94603b974c1218423440
Esteban Demarchi <83315171+EstebanDem@users.noreply.github.com>
2024-08-21 21:27:50 -0300

    :sparkles: Add get employee salary use case

---
+++ internal/application/usecases/getemployeesalary_usecase.go
@@ -0,0 +1,73 @@
+package usecases
+
+import (
+	"github.com/google/uuid"
+	"go-clean-arch-example/internal/application/services"
+	"go-clean-arch-example/internal/domain"
+)
+
+type GetEmployeeSalaryRequest struct {
+	EmployeeId uuid.UUID
+	Currency   string
+}
+
+type GetEmployeeSalaryResponse struct {
+	EmployeeId      uuid.UUID
+	Salary          Salary
+	ConvertedSalary ConvertedSalary
+}
+
+type Salary struct {
+	Currency string
+	Value    float64
+}
+
+type ConvertedSalary struct {
+	Currency string
+	Value    float64
+	Rate     float64
+}
+
+type GetEmployeeSalaryUseCase interface {
+	GetSalary(request GetEmployeeSalaryRequest) (GetEmployeeSalaryResponse, error)
+}
+
+type getEmployeeSalaryUseCase struct {
+	employeesRepo     domain.EmployeeRepository
+	currencyConverter services.CurrencyConverter
+}
+
+func (g getEmployeeSalaryUseCase) GetSalary(request GetEmployeeSalaryRequest) (GetEmployeeSalaryResponse, error) {
+	employee, err := g.employeesRepo.GetById(request.EmployeeId)
+	if err != nil {
+		return GetEmployeeSalaryResponse{}, err
+	}
+
+	ratio, err := g.currencyConverter.GetExchangeRate(employee.Salary.Currency, request.Currency)
+	if err != nil {
+		return GetEmployeeSalaryResponse{}, err
+	}
+
+	convertedSalary := ratio * employee.Salary.Value
+
+	return GetEmployeeSalaryResponse{
+		EmployeeId: employee.Id,
+		Salary: Salary{
+			Currency: employee.Salary.Currency,
+			Value:    employee.Salary.Value,
+		},
+		ConvertedSalary: ConvertedSalary{
+			Currency: request.Currency,
+			Value:    convertedSalary,
+			Rate:     ratio,
+		},
+	}, nil
+
+}
+
+func InitGetEmployeeSalaryUseCase(er domain.EmployeeRepository, cc services.CurrencyConverter) GetEmployeeSalaryUseCase {
+	return getEmployeeSalaryUseCase{
+		employeesRepo:     er,
+		currencyConverter: cc,
+	}
+}
