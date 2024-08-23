85a32552041cd18004ed40a60326b5bc1f433ec7
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2024-08-22 21:31:09 -0300

    Add salary http handler and inject it into app

--- internal/infrastructure/framework/application.go
+++ internal/infrastructure/framework/application.go
@@ -3,8 +3,10 @@ package framework
 import (
 	"go-clean-arch-example/internal/application/usecases"
 	"go-clean-arch-example/internal/infrastructure/inputports/http/handler"
+	"go-clean-arch-example/internal/infrastructure/interfaceadapters/restclients"
 	"go-clean-arch-example/internal/infrastructure/interfaceadapters/storage/memory"
 	"net/http"
+	"os"
 )
 
 func NewApp() *http.ServeMux {
@@ -13,11 +15,16 @@ func NewApp() *http.ServeMux {
 	// repositories
 	repo := memory.NewInMemoryEmployeeRepository()
 
+	// clients
+	freeCurrencyApiClient := restclients.NewFreeCurrencyApiClient(os.Getenv("API_KEY"))
+
 	// use-cases
 	ucAddEmployee := usecases.InitEmployeeUseCase(repo)
+	ucGetEmployeeSalary := usecases.InitGetEmployeeSalaryUseCase(repo, freeCurrencyApiClient)
 
 	// routes
 	mux.HandleFunc("POST /v1/employees", handler.AddEmployeeHandler(ucAddEmployee))
+	mux.HandleFunc("GET /v1/employees/{id}/salary/{currency}", handler.GetEmployeeSalaryHandler(ucGetEmployeeSalary))
 
 	return mux
 }
---
+++ internal/infrastructure/inputports/http/handler/getemployeesalary_handler.go
@@ -0,0 +1,81 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"go-clean-arch-example/internal/application/usecases"
+	"net/http"
+)
+
+type GetEmployeeSalaryJsonResponse struct {
+	EmployeeId      uuid.UUID       `json:"employee_id"`
+	Salary          Salary          `json:"salary"`
+	ConvertedSalary ConvertedSalary `json:"converted_salary"`
+}
+
+type Salary struct {
+	Currency string  `json:"currency"`
+	Value    float64 `json:"value"`
+}
+
+type ConvertedSalary struct {
+	Currency string  `json:"currency"`
+	Value    float64 `json:"value"`
+	Rate     float64 `json:"rate"`
+}
+
+func GetEmployeeSalaryHandler(uc usecases.GetEmployeeSalaryUseCase) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		currency := r.PathValue("currency")
+		if id == "" || currency == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err := uuid.Validate(id)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		employeeRequest := usecases.GetEmployeeSalaryRequest{
+			EmployeeId: uuid.MustParse(id),
+			Currency:   currency,
+		}
+
+		employeeResponse, err := uc.GetSalary(employeeRequest)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		jsonResponse, err := json.Marshal(toGetEmployeeSalaryJsonResponse(employeeResponse))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusCreated)
+		if _, err := w.Write(jsonResponse); err != nil {
+			return
+		}
+
+	}
+}
+
+func toGetEmployeeSalaryJsonResponse(er usecases.GetEmployeeSalaryResponse) GetEmployeeSalaryJsonResponse {
+	return GetEmployeeSalaryJsonResponse{
+		EmployeeId: er.EmployeeId,
+		Salary: Salary{
+			Currency: er.Salary.Currency,
+			Value:    er.Salary.Value,
+		},
+		ConvertedSalary: ConvertedSalary{
+			Currency: er.ConvertedSalary.Currency,
+			Value:    er.ConvertedSalary.Value,
+			Rate:     er.ConvertedSalary.Rate,
+		},
+	}
+}
