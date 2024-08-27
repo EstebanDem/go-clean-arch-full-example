0573fa2551c3de968fa081a6c61a0c2eb22e7705
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2024-08-26 21:06:23 -0300

    :technologist: Flag to select storage type

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"flag"
 	"go-clean-arch-example/internal/infrastructure/framework"
 	"net/http"
 )
 
 func main() {
-	app := framework.NewApp()
+	storage := flag.String("storage", "memory", "Database type to use (mysql, mongo or memory)")
+	flag.Parse()
+	app := framework.NewApp(*storage)
 
 	err := http.ListenAndServe(":9099", app)
 	if err != nil {
 		panic("Error starting application")
 	}
-
 }
--- internal/infrastructure/framework/application.go
+++ internal/infrastructure/framework/application.go
@@ -2,27 +2,22 @@ package framework
 
 import (
 	"go-clean-arch-example/internal/application/usecases"
+	"go-clean-arch-example/internal/domain"
 	"go-clean-arch-example/internal/infrastructure/inputports/http/handler"
 	"go-clean-arch-example/internal/infrastructure/interfaceadapters/restclients"
+	"go-clean-arch-example/internal/infrastructure/interfaceadapters/storage/memory"
 	"go-clean-arch-example/internal/infrastructure/interfaceadapters/storage/mongodb"
+	"go-clean-arch-example/internal/infrastructure/interfaceadapters/storage/mysql"
 	"log"
 	"net/http"
 	"os"
 )
 
-func NewApp() *http.ServeMux {
+func NewApp(storage string) *http.ServeMux {
 	mux := http.NewServeMux()
 
 	// repositories
-	//repo := memory.NewInMemoryEmployeeRepository()
-
-	/*	repo, err := mysql.NewMySqlEmployeeRepository()
-		if err != nil {
-			log.Fatal(err.Error())
-			return nil
-		}*/
-
-	repo, err := mongodb.NewEmployeeRepositoryMongo()
+	repo, err := initEmployeeRepository(storage)
 	if err != nil {
 		log.Fatal(err.Error())
 		return nil
@@ -40,4 +35,16 @@ func NewApp() *http.ServeMux {
 	mux.HandleFunc("GET /v1/employees/{id}/salary/{currency}", handler.GetEmployeeSalaryHandler(ucGetEmployeeSalary))
 
 	return mux
+}
+
+func initEmployeeRepository(storage string) (domain.EmployeeRepository, error) {
+	switch storage {
+	case "mysql":
+		return mysql.NewMySqlEmployeeRepository()
+	case "mongo":
+		return mongodb.NewEmployeeRepositoryMongo()
+	default:
+		return memory.NewInMemoryEmployeeRepository(), nil
+	}
+
 }
