94c143a9b0277045c838458e6fbdd7a74555778e
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2024-09-21 16:40:20 -0300

    :art: Format sql schema

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -4,13 +4,13 @@ CREATE TABLE IF NOT EXISTS salary (
     wage DECIMAL(15, 2) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS employee (sql: Scan error on column index 5, name "created_at": unsupported Scan, storing driver.Value type []uint8 into type *time.Time
-                                        id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                                        uuid CHAR(36) NOT NULL, -- UUID almacenado como cadena
+CREATE TABLE IF NOT EXISTS employee (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    uuid CHAR(36) NOT NULL,
     Name VARCHAR(64) NOT NULL,
     salary_id BIGINT UNSIGNED,
     country VARCHAR(30) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (salary_id) REFERENCES salary(id) ON DELETE SET NULL
-    );
+);
